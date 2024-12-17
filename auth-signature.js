4696e8c71264df326d53d80a171e714c125801b3
akmalsyrf <dev.akmal69@gmail.com>
2024-12-17 09:25:19 +0700

    feat: add postman collection

--- auth-signature.js
+++ auth-signature.js
@@ -13,33 +13,40 @@ module.exports = async (req, res, next) => {
   const partnerId = req.headers['x-partner-id'] || undefined;
   const timestamp = req.headers['x-timestamp'] || undefined;
   const signature = req.headers['x-signature'] || undefined;
-  const accessToken = req.headers.authorization.replace('Bearer ', '') || undefined;
+  const accessToken = (req.headers.authorization && req.headers.authorization.replace('Bearer ', '')) || undefined;
   const endpointUrl = getPath(req.url)
   const urlCode = req.url === '/inquiry' ? '24' : '25'
 
-  const decryptedKey = Buffer.from(partnerId, 'base64').toString();
-  const splitKey = decryptedKey.split(':');
-  if (splitKey[0] !== briKey.publicKey || splitKey[1] !== briKey.privateKey) {
-    return res.status(400).send({
-      responseCode: `${400}${urlCode}00`,
-      responseMessage: 'Unauthorized.'
-    })
-  }
-
-  const hash = CryptoJS.SHA256(JSON.stringify(req.body), null, 0);
-
-  const stringToSign = `${method}:${endpointUrl}:${accessToken}:${hash.toString(CryptoJS.enc.Hex)}:${timestamp}`;
-  const hmacSignature = CryptoJS.enc.Hex.stringify(CryptoJS.HmacSHA512(stringToSign, briKey.clientSecret));
-
-  console.log('Signature BRI Auth', signature.toLowerCase())
-  console.log('HMAC Signature BRI Auth', hmacSignature.toLowerCase())
-
-  if (hmacSignature.toLowerCase() !== signature.toLowerCase()) {
+  if (partnerId && timestamp && signature && accessToken) {
+    const decryptedKey = Buffer.from(partnerId, 'base64').toString();
+    const splitKey = decryptedKey.split(':');
+    if (splitKey[0] !== briKey.publicKey || splitKey[1] !== briKey.privateKey) {
+      return res.status(400).send({
+        responseCode: `${400}${urlCode}00`,
+        responseMessage: 'Unauthorized.'
+      })
+    }
+  
+    const hash = CryptoJS.SHA256(JSON.stringify(req.body), null, 0);
+  
+    const stringToSign = `${method}:${endpointUrl}:${accessToken}:${hash.toString(CryptoJS.enc.Hex)}:${timestamp}`;
+    const hmacSignature = CryptoJS.enc.Hex.stringify(CryptoJS.HmacSHA512(stringToSign, briKey.clientSecret));
+  
+    console.log('Signature BRI Auth', signature.toLowerCase())
+    console.log('HMAC Signature BRI Auth', hmacSignature.toLowerCase())
+  
+    if (hmacSignature.toLowerCase() !== signature.toLowerCase()) {
+      return res.status(401).send({
+        responseCode: `${401}${urlCode}01`,
+        responseMessage: 'Invalid Token (B2B)'
+      })
+    }
+  
+    return next();
+  } else {
     return res.status(401).send({
       responseCode: `${401}${urlCode}01`,
       responseMessage: 'Invalid Token (B2B)'
     })
   }
-
-  return next();
 }
--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const app = express()
 const PORT = 3000
 
 app.get("/", (_, res) => res.send("Hello world"))
-app.use((_, res) => {
-    res.status(404).json({ error: 'Not Found', message: 'The requested resource was not found on this server.' });
-});
 
 const authSignature = require("./auth-signature")
 
-app.post("/inquiry", authSignature, (req, res) => res.json(req.body))
-app.post("/pay", authSignature, (req, res) => res.json(req.body))
+app.post("/snap/access-token", authSignature, (_, __) => {})
+app.post("/snap/transfer-va/inquiry", authSignature, (req, res) => res.json(req.body))
+app.post("/snap/transfer-va/payment", authSignature, (req, res) => res.json(req.body))
+app.use((_, res) => {
+    res.status(404).json({ error: 'Not Found', message: 'The requested resource was not found on this server.' })
+})
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
